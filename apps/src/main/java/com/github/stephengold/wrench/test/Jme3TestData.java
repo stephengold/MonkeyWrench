41c8cbd414017a7b36183b75d4f3d4bd2efb8a20
Stephen Gold <sgold@sonic.net>
2023-09-15 13:05:11 -0700

    apps: add the Jme3TestData class

---
+++ apps/src/main/java/com/github/stephengold/wrench/test/Jme3TestData.java
@@ -0,0 +1,289 @@
+/*
+ Copyright (c) 2023, Stephen Gold
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+
+ 1. Redistributions of source code must retain the above copyright notice, this
+    list of conditions and the following disclaimer.
+
+ 2. Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+
+ 3. Neither the name of the copyright holder nor the names of its
+    contributors may be used to endorse or promote products derived from
+    this software without specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.github.stephengold.wrench.test;
+
+import java.util.logging.Logger;
+
+/**
+ * Utility methods to access the models/scenes in jme3-testdata.
+ *
+ * @author Stephen Gold sgold@sonic.net
+ */
+final class Jme3TestData {
+    // *************************************************************************
+    // constants and loggers
+
+    /**
+     * message logger for this class
+     */
+    final static Logger logger = Logger.getLogger(Jme3TestData.class.getName());
+    /**
+     * names of the models/scenes in jme3-testdata v3.1.0, in ascending
+     * lexicographic order
+     */
+    final private static String[] names310 = {
+        "AnimTest", "BaseMesh249", "BaseMesh256", "BaseScene", "BasicCubeLow",
+        "Boat", "Constraints", "Curves", "Elephant", "Ferrari", "HoverTank",
+        "Kerrigan", "ManyLights", "Materials", "Modifiers", "MonkeyHead",
+        "MountainValley", "Ninja", "Nurbs", "ObjectAnimation", "Particles",
+        "Positions", "Rocket", "SignPost", "SimpleAnimation", "SinbadBlend",
+        "SinbadXml", "Sword", "TeapotObj", "TeapotXml", "Terrain",
+        "TexturedPlane", "Textures", "Tree", "WaterTest"
+    };
+    /**
+     * names of the models/scenes in jme3-testdata v3.6.1, in ascending
+     * lexicographic order
+     */
+    final private static String[] names361 = {
+        "BasicCubeLow", "Boat", "Box", "Duck", "Elephant", "Ferrari",
+        "HoverTank", "ManyLights", "MonkeyHead", "Ninja", "PbrRef", "Rocket",
+        "SignPost", "SinbadXml", "Sword", "TeapotObj", "TeapotXml", "Terrain",
+        "Tree", "TwoChairs", "WaterTest"
+    };
+    // *************************************************************************
+    // constructors
+
+    /**
+     * A private constructor to inhibit instantiation of this class.
+     */
+    private Jme3TestData() {
+        // do nothing
+    }
+    // *************************************************************************
+    // new methods exposed
+
+    /**
+     * Return the asset path to the selected C-G model.
+     *
+     * @param modelName the name of the model/scene (not null)
+     * @return the asset path, or null if the name is not recognized
+     */
+    static String assetPath(String modelName) {
+        String result;
+        switch (modelName) {
+            case "AnimTest": // removed from v3.4.0
+                result = "Blender/2.4x/animtest.blend";
+                break;
+
+            case "BaseMesh249": // removed from v3.4.0
+                result = "Blender/2.4x/BaseMesh_249.blend";
+                break;
+
+            case "BaseMesh256": // removed from v3.4.0
+                result = "Blender/2.5x/BaseMesh_256.blend";
+                break;
+
+            case "BaseScene": // removed from v3.4.0
+                result = "Blender/2.4x/BaseScene.blend";
+                break;
+
+            case "BasicCubeLow":
+                result = "Models/Test/BasicCubeLow.obj";
+                break;
+
+            case "Boat":
+                result = "Models/Boat/boat.mesh.xml";
+                break;
+
+            case "Box": // added in v3.2.0
+                result = "Models/gltf/box/box.gltf";
+                break;
+
+            case "Constraints": // removed from v3.4.0
+                result = "Blender/2.4x/constraints.blend";
+                break;
+
+            case "Curves": // removed from v3.4.0
+                result = "Blender/2.4x/curves.blend";
+                break;
+
+            case "Duck": // added in v3.2.0
+                result = "Models/gltf/duck/Duck.gltf";
+                break;
+
+            case "Elephant":
+                // TODO Assimp doesn't recognize materials in .j3m files.
+                result = "Models/Elephant/Elephant.mesh.xml";
+                break;
+
+            case "Ferrari":
+                /*
+                 * TODO Assimp doesn't recognize the .scene file extension
+                 * and has no suitable reader for the format.
+                 */
+                result = "Models/Ferrari/Car.scene";
+                break;
+
+            case "HoverTank":
+                // TODO Assimp doesn't recognize materials in .j3m files.
+                result = "Models/HoverTank/Tank2.mesh.xml";
+                break;
+
+            case "Kerrigan": // removed from v3.4.0
+                result = "Blender/2.4x/kerrigan.blend";
+                break;
+
+            case "ManyLights":
+                /*
+                 * TODO Assimp doesn't recognize the .scene file extension
+                 * and has no suitable reader for the format.
+                 */
+                result = "Scenes/ManyLights/Main.scene";
+                break;
+
+            case "Materials": // removed from v3.4.0
+                result = "Blender/2.4x/materials.blend";
+                break;
+
+            case "Modifiers": // removed from v3.4.0
+                result = "Blender/2.4x/modifiers.blend";
+                break;
+
+            case "MonkeyHead":
+                // TODO Assimp doesn't recognize materials in .j3m files.
+                result = "Models/MonkeyHead/MonkeyHead.mesh.xml";
+                break;
+
+            case "MountainValley": // removed from v3.4.0
+                result = "Blender/2.4x/MountainValley_Track.blend";
+                break;
+
+            case "Ninja":
+                /*
+                 * TODO Assimp ignores the 4th argument of "specular"
+                 * which apparently indicates shininess.
+                 */
+                result = "Models/Ninja/Ninja.mesh.xml";
+                break;
+
+            case "Nurbs": // removed from v3.4.0
+                result = "Blender/2.4x/nurbs.blend";
+                break;
+
+            case "ObjectAnimation": // removed from v3.4.0
+                result = "Blender/2.4x/ObjectAnimation.blend";
+                break;
+
+            case "Oto": // TODO possible infinite loop in Assimp!
+                result = "Models/Oto/Oto.mesh.xml";
+                break;
+
+            case "Particles": // removed from v3.4.0
+                result = "Blender/2.4x/particles.blend";
+                break;
+
+            case "PbrRef": // added in v3.2.0
+                result = "Scenes/PBR/ref/scene.gltf";
+                break;
+
+            case "Positions": // removed from v3.4.0
+                result = "Blender/2.4x/positions.blend";
+                break;
+
+            case "Rocket":
+                result = "Models/SpaceCraft/Rocket.mesh.xml";
+                break;
+
+            case "SignPost":
+                result = "Models/Sign Post/Sign Post.mesh.xml";
+                break;
+
+            case "SimpleAnimation": // removed from v3.4.0
+                result = "Blender/2.4x/SimpleAnimation.blend";
+                break;
+
+            case "SinbadBlend": // removed from v3.4.0
+                result = "Blender/2.4x/Sinbad.blend";
+                break;
+
+            case "SinbadXml":
+                /*
+                 * TODO Assimp ignores the 4th argument of "specular"
+                 * which apparently indicates shininess.
+                 */
+                result = "Models/Sinbad/Sinbad.mesh.xml";
+                break;
+
+            case "Sword":
+                result = "Models/Sinbad/Sword.mesh.xml";
+                break;
+
+            case "TeapotObj":
+                result = "Models/Teapot/Teapot.obj";
+                break;
+
+            case "TeapotXml":
+                result = "Models/Teapot/Teapot.mesh.xml";
+                break;
+
+            case "Terrain":
+                result = "Models/Terrain/Terrain.mesh.xml";
+                break;
+
+            case "TexturedPlane": // removed from v3.4.0
+                result = "Blender/2.4x/texturedPlaneTest.blend";
+                break;
+
+            case "Textures": // removed from v3.4.0
+                result = "Blender/2.4x/textures.blend";
+                break;
+
+            case "Tree":
+                // TODO Assimp doesn't recognize materials in .j3m files.
+                result = "Models/Tree/Tree.mesh.xml";
+                break;
+
+            case "TwoChairs":
+                // added in v3.4.0
+                result = "OBJLoaderTest/TwoChairs.obj";
+                break;
+
+            case "WaterTest":
+                result = "Models/WaterTest/WaterTest.mesh.xml";
+                break;
+
+            default:
+                result = null;
+        }
+
+        return result;
+    }
+
+    /**
+     * Enumerate the model/scene names.
+     *
+     * @param pre34 true for old (pre-v3.4) jme3-testdata, false for current one
+     * @return a pre-existing array of model/scene names (not null, all elements
+     * non-null, in ascending lexicographic order)
+     */
+    static String[] listModels(boolean pre34) {
+        String[] result = pre34 ? names310 : names361;
+        return result;
+    }
+}
