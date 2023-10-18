ea925e2239ac1618bca98e818e2ec0c9dc1d33ab
Stephen Gold <sgold@sonic.net>
2023-10-18 15:54:31 -0700

    LwjglReader:  re-order methods

--- lib/src/main/java/com/github/stephengold/wrench/LwjglReader.java
+++ lib/src/main/java/com/github/stephengold/wrench/LwjglReader.java
@@ -77,6 +77,65 @@ final public class LwjglReader {
     // new methods exposed
 
     /**
+     * Count the meshes in the specified AINode and all its descendants. Note:
+     * recursive!
+     *
+     * @param aiNode the node to process (not null, unaffected)
+     * @return the count (&ge;0)
+     */
+    static int countMeshesInSubtree(AINode aiNode) {
+        int result = aiNode.mNumMeshes();
+
+        int numChildren = aiNode.mNumChildren();
+        if (numChildren > 0) {
+            PointerBuffer pChildren = aiNode.mChildren();
+            for (int childIndex = 0; childIndex < numChildren; ++childIndex) {
+                long handle = pChildren.get(childIndex);
+                AINode aiChild = AINode.createSafe(handle);
+                result += countMeshesInSubtree(aiChild);
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * Print the specified tree of Assimp nodes to {@code System.out}. Note:
+     * recursive!
+     *
+     * @param aiNode the node to start from (not null, unaffected)
+     * @param indent the indent text (not null, may be empty)
+     */
+    static void dumpNodes(AINode aiNode, String indent) {
+        System.out.print(indent);
+
+        String name = aiNode.mName().dataString();
+        System.out.print(MyString.quote(name));
+
+        AIMatrix4x4 aiMatrix = aiNode.mTransformation();
+        Matrix4f t = ConversionUtils.convertMatrix(aiMatrix);
+        System.out.printf(" [%s %s %s %s]",
+                MyString.describe(t.m00), MyString.describe(t.m01),
+                MyString.describe(t.m02), MyString.describe(t.m03));
+        System.out.printf(" [%s %s %s %s]",
+                MyString.describe(t.m10), MyString.describe(t.m11),
+                MyString.describe(t.m12), MyString.describe(t.m13));
+        System.out.printf(" [%s %s %s %s]%n",
+                MyString.describe(t.m20), MyString.describe(t.m21),
+                MyString.describe(t.m22), MyString.describe(t.m23));
+
+        int numChildren = aiNode.mNumChildren();
+        if (numChildren > 0) {
+            PointerBuffer pChildren = aiNode.mChildren();
+            for (int childIndex = 0; childIndex < numChildren; ++childIndex) {
+                long handle = pChildren.get(childIndex);
+                AINode aiChild = AINode.createSafe(handle);
+                dumpNodes(aiChild, indent + "  ");
+            }
+        }
+    }
+
+    /**
      * Log importer progress to the standard output.
      * <p>
      * Remember to invoke {@code Assimp.aiDetachAllLogStreams()} when done
@@ -171,64 +230,5 @@ final public class LwjglReader {
         }
 
         return result;
-    }
-
-    /**
-     * Count the meshes in the specified AINode and all its descendants. Note:
-     * recursive!
-     *
-     * @param aiNode the node to process (not null, unaffected)
-     * @return the count (&ge;0)
-     */
-    static int countMeshesInSubtree(AINode aiNode) {
-        int result = aiNode.mNumMeshes();
-
-        int numChildren = aiNode.mNumChildren();
-        if (numChildren > 0) {
-            PointerBuffer pChildren = aiNode.mChildren();
-            for (int childIndex = 0; childIndex < numChildren; ++childIndex) {
-                long handle = pChildren.get(childIndex);
-                AINode aiChild = AINode.createSafe(handle);
-                result += countMeshesInSubtree(aiChild);
-            }
-        }
-
-        return result;
-    }
-
-    /**
-     * Print the specified tree of Assimp nodes to {@code System.out}. Note:
-     * recursive!
-     *
-     * @param aiNode the node to start from (not null, unaffected)
-     * @param indent the indent text (not null, may be empty)
-     */
-    static void dumpNodes(AINode aiNode, String indent) {
-        System.out.print(indent);
-
-        String name = aiNode.mName().dataString();
-        System.out.print(MyString.quote(name));
-
-        AIMatrix4x4 aiMatrix = aiNode.mTransformation();
-        Matrix4f t = ConversionUtils.convertMatrix(aiMatrix);
-        System.out.printf(" [%s %s %s %s]",
-                MyString.describe(t.m00), MyString.describe(t.m01),
-                MyString.describe(t.m02), MyString.describe(t.m03));
-        System.out.printf(" [%s %s %s %s]",
-                MyString.describe(t.m10), MyString.describe(t.m11),
-                MyString.describe(t.m12), MyString.describe(t.m13));
-        System.out.printf(" [%s %s %s %s]%n",
-                MyString.describe(t.m20), MyString.describe(t.m21),
-                MyString.describe(t.m22), MyString.describe(t.m23));
-
-        int numChildren = aiNode.mNumChildren();
-        if (numChildren > 0) {
-            PointerBuffer pChildren = aiNode.mChildren();
-            for (int childIndex = 0; childIndex < numChildren; ++childIndex) {
-                long handle = pChildren.get(childIndex);
-                AINode aiChild = AINode.createSafe(handle);
-                dumpNodes(aiChild, indent + "  ");
-            }
-        }
     }
 }
