8f51329d79e1c39a08e8293a3d29c16b19c7529f
Stephen Gold <sgold@sonic.net>
2023-10-22 23:26:55 -0700

    bump version to 0.5.0 and create release-log.md

---
+++ lib/release-log.md
@@ -0,0 +1,5 @@
+# release log for the MonkeyWrench library
+
+## Version 0.5.0 released on TBD
+
+This was the initial baseline release for testing.
--- lib/src/main/java/com/github/stephengold/wrench/LwjglReader.java
+++ lib/src/main/java/com/github/stephengold/wrench/LwjglReader.java
@@ -242,6 +242,6 @@ final public class LwjglReader {
      * @return a release name or a snapshot name (not null, not empty)
      */
     public static String version() {
-        return "0.1.0-SNAPSHOT";
+        return "0.5.0";
     }
 }
