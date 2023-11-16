878cea21356357a89e86736925f0f0bda7bb3107
Stephen Gold <sgold@sonic.net>
2023-11-16 13:44:13 -0800

    add the "extract3js.sh" script

---
+++ bash/extract3js.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+NAME=three.js-r158
+/usr/bin/unzip -q ~/Downloads/${NAME}.zip ${NAME}/examples/models/\*
+
+/usr/bin/mkdir -p downloads/threejs
+/usr/bin/mv ${NAME}/examples/models/{3ds,3mf,bvh,collada,fbx,gltf,lwo,obj,ply,stl} \
+    downloads/threejs
+
+# cleanup:
+/usr/bin/rm -rf ${NAME}
